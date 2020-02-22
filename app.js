87955d0d142fbeb6036084fed0865b2ee0f33256
zafra89 <51385853+zafra89@users.noreply.github.com>
2020-02-22 14:07:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-class SpeedTypingGame {
+lass SpeedTypingGame {
     constructor(totalTime) {
         this.totalTime = totalTime;
         this.timeRemaining = totalTime;
         this.timer = document.getElementById('seconds');
         this.startBtn = document.getElementById('start-btn');
         this.nextBtn = document.getElementById('next-btn');
-        this.texts = ['hola', 'adios', 'buenas', 'arigato', 'byebye', 'namaste'];
+        this.restartBtn = document.getElementById('restart-btn');
+        this.textarea = document.getElementById('textarea');
+        this.texts = ['hola', 'adios', 'goodbye', 'hello'];
         this.textToType = document.getElementById('text-to-type');
         this.currentIndex = 0;
     }
@@ -29,13 +31,32 @@ class SpeedTypingGame {
         clearInterval(this.countdown);
         this.timer.textContent = `TIME'S UP`;
     }
+    textTesting() {
+        if (this.textarea.value === this.textToType.innerText)
+        this.victory();
+    }
     victory() {
         clearInterval(this.countdown);
         this.timer.textContent = `CORRECT`;
+        this.nextBtn.classList.remove('hide');
+        this.textToType.classList.add('hide');
     }
-    shuffleText(texts) {
-        texts.sort(() => Math.random() - .5);
-        this.textToType.innerText = texts[this.currentIndex];
+    shuffleText(x) {
+        x.sort(() => Math.random() - .5);
+        this.textToType.innerText = x[this.currentIndex];
+    }
+    nextText() {
+        if (this.textToType.innerText === 'undefined') {
+            this.restartBtn.classList.remove('hide');
+        } else {
+            this.currentIndex++;
+        }
+        this.resetState();
+    }
+    resetState() {
+        this.textToType.classList.remove('hide');
+        this.textToType.innerText = this.texts[this.currentIndex];
+        this.textarea.value = '';
     }
 }
 
@@ -48,11 +69,27 @@ if (document.readyState == 'loading') {
 function ready() {
     let startBtn = document.getElementById('start-btn');
     let textToType = document.getElementById('text-to-type');
-    let stg = new SpeedTypingGame(5);
+    let nextBtn = document.getElementById('next-btn');
+    let restartBtn = document.getElementById('restart-btn');
+    let textarea = document.getElementById('textarea');
+    let stg = new SpeedTypingGame(10);
     
     startBtn.addEventListener('click', () => {
         startBtn.classList.add('hide');
         textToType.classList.remove('hide');
         stg.startGame();
     });
+    
+    textarea.addEventListener('keyup', () => {
+        stg.textTesting();
+    });
+
+    nextBtn.addEventListener('click', () => {
+        nextBtn.classList.add('hide');
+        stg.nextText();
+    });
+    restartBtn.addEventListener('click', () => {
+        restartBtn.classList.add('hide');
+        stg.startGame();
+    })
 }
